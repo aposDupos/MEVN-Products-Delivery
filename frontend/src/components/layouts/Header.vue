e4560c042f67668bfe1b44ca2a63ec210b3f7cf3
Vlad Tyrtychnyi <tva100702@gmail.com>
2021-05-29 22:24:22 +0300

    Base frontend structure added

---
+++ frontend/src/components/layouts/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header">
+    <div class="header__left">LOGO</div>
+    <nav class="nav">
+      <ul class="nav-list">
+        <li class="nav-list__item" v-for="({id, title}, key) in categories" :key="key">
+          <router-link :to="`category/${id}`">{{title}}</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    categories: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
